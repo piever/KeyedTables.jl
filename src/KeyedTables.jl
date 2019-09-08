c25f5c3e96f8adf6f01a65e515f18c602d86e974
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-09-08 16:33:41 +0100

    Files generated by PkgTemplates

---
+++ src/KeyedTables.jl
@@ -0,0 +1,5 @@
+module KeyedTables
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using KeyedTables
+using Test
+
+@testset "KeyedTables.jl" begin
+    # Write your own tests here.
+end
